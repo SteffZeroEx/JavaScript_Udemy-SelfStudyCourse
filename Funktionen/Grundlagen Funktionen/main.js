7f30418b21945d8fedc983927911c1709716d077
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-12 10:49:11 +0200

    # Grundlagen Funktion gelernt

---
+++ Funktionen/Grundlagen Funktionen/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Funktions-Ausdruck (anonyme Funktion)
+/*
+const meine_funktion = function () {
+    auszuführender Code
+};
+
+meine_funktion(); // Funktionsaufruf - Um die aktuelle Funktion aufzurufen
+*/
+
+const log_it = function () {
+  console.log("Funktionen sind voll super!")  
+};
+
+log_it();
+log_it();
+log_it();
+log_it();
+log_it();
+log_it();
+log_it();
+log_it();
+log_it();
+log_it();
