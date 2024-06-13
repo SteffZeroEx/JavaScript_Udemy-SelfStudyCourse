c467425d3055b100a36b21d42915acb405e4cb6d
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-13 12:44:15 +0200

    # Grundlagen von Objekten gelernt

---
+++ Objekte/Grundlagen von Objekten/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+let iban = "DE6941247506874125";
+let bic = "NOLADE55SKI";
+let kontostand = 3500;
+let aktiv = true;
+*/
+
+let konto_1 = {
+  iban: "DE6941247506874125",
+  bic: "NOLADE55SKI",
+  kontostand: 3500,
+  aktiv: true
+};
+console.log(konto_1);
+
+let konto_2 = {
+  iban: "DE694124750687875",
+  bic: "NOLADE55SKI",
+  kontostand: 5500,
+  aktiv: true
+};
+
+console.log(konto_2);
