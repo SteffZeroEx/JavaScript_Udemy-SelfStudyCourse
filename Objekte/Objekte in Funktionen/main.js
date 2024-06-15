bef374a7eed84c2f4737c0dfc912d37475864751
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-15 16:48:33 +0200

    # Objekte in Funktionen- eigene Übung

--- Objekte/Objekte in Funktionen/main.js
+++ Objekte/Objekte in Funktionen/main.js
@@ -35,3 +35,23 @@ kontostand_ausgeben(konto_1);
 kontostand_ausgeben(konto_2);
 
 // Challenge: Denkt euch noch ein paar hilfreiche Funktionen für unsere Konten aus.
+
+const aktivität = function (konto) {
+  console.log(`Das Konto von ${konto.inhaber.vorname} ${konto.inhaber.nachname} steht auf Aktiv: ${konto.aktiv}.`);
+};
+aktivität(konto_1);
+aktivität(konto_2);
+
+console.log("------------------------------------------");
+
+const alterspruefung = function (konto) {
+  console.log(`Zur Identifikation des Kontoinhabers benötigen wir das Alter. Der Kontoinhaber ${konto.inhaber.vorname} ${konto.inhaber.nachname} ist ${konto.inhaber.alter} Jahre alt.`);
+  if (konto.inhaber.alter >= 18) {
+    console.log(`${konto.inhaber.vorname} ${konto.inhaber.nachname} ist berechtigt, selbstständig über das Konto zu verfügen.`);
+  } else {
+    console.log(`${konto.inhaber.vorname} ${konto.inhaber.nachname} ist noch nicht berechtigt, selbstständig über das Konto zu verfügen. Kontaktiere hierzu bitte deine Eltern!`);
+  }
+};
+alterspruefung(konto_1);
+alterspruefung(konto_2);
+
