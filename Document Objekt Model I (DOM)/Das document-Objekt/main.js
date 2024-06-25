15d6734f8ce8b840573d7d4f4e18d6d9241535fb
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-25 17:44:58 +0200

    # Kurze Beschreibung hinzugefügt inkl. Verlinkung

--- Document Objekt Model I (DOM)/Das document-Objekt/main.js
+++ Document Objekt Model I (DOM)/Das document-Objekt/main.js
@@ -1,7 +1,8 @@
 "use strict";
 
-console.log(document);
-
+console.log(document);  // Das document-Objekt bezieht sich auf den Inhalt, der in einem Browser-Fenster angezeigt wird. 
+                        // Der Browser erzeugt beim Laden des Inhalts genau ein document-Objekt und stellt es in einer Eigenschaft namens "document" des globalen Window-Objekts zur Verfügung.
+                        // Weitere Infos: https://wiki.selfhtml.org/wiki/JavaScript/DOM/Document oder https://developer.mozilla.org/en-US/docs/Web/API/Document?retiredLocale=de
 
 console.log(document.head);
 
