da9ed8b2dc49c09aefcc9dcf173b98291f567c2f
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-26 13:29:52 +0200

    # Die Eigenschaften innerHTML, innerText und textContent Fortsetzung

--- Document Objekt Model I (DOM)/Die Eigenschaften innerHTML, innerText und textContent/main.js
+++ Document Objekt Model I (DOM)/Die Eigenschaften innerHTML, innerText und textContent/main.js
@@ -3,9 +3,9 @@
 let jumbo = document.querySelector(".jumbotron");
 // console.log(jumbo);
 
-console.log(jumbo.innerHTML);          
-console.log(jumbo.innerText);           // 
-console.log(jumbo.textContent);
+// console.log(jumbo.innerHTML);          
+// console.log(jumbo.innerText);           
+// console.log(jumbo.textContent);
 
 // innerHTML:
  // Die Eigenschaft innerHTML gibt uns die HTML-Struktur in einem Element inklusive aller Texte wieder, genauso wie sie im Quelltext stehen würde.
@@ -20,3 +20,26 @@ console.log(jumbo.textContent);
  // Gibt uns den Text eines HTML-Elements und seiner Kindelemente und nachfahrenden Elemente zurück, allerdings ohne Rücksicht auf
  //  den gerenderten Text im Frontend, sondern es wird viel mehr des gesamte Text aus dem HTML-Elementen genommen und dargestellt, ohne
  //  die umgebenen HTML-Elemente
+
+
+
+ // Texte/Inhalt setzen
+
+ let string_1 = "";
+ let string_2 = "<header><h2>Lorem ipsum dolor sit amet</h2></header>";
+ let string_3 = "<p> Lorem ipsum dolor sit amet.</p>";
+ let string_4 = "Lorem ipsum dolor sit amet.";
+
+//  jumbo.innerHTML = string_1;
+// jumbo.innerHTML = string_2;
+// jumbo.innerHTML += string_3;                // Text zum vorhandenen Text hinzufügen inklusive eines Paragraphen/Absatzes (p)
+// jumbo.innerHTML += string_4;                // Geht auch, wie bei string_3, aber nicht so schön, da nur purer text in der HTML steht. dann lieber einen DOM-String wie bei string_3
+
+//  jumbo.innerText = string_1;
+// jumbo.innerText = string_2;                 // mit innerText einen DOM-String einsetzen funktioniert nicht. innerText gibt und ja logischerweise nur die Textinhalte zurück
+// jumbo.innerText += string_3;                // Konkatination auch nicht möglich. Siehe innerText string_2
+
+
+// jumbo.textContent = string_1;
+// jumbo.textContent = string_2;               // Das gleiche wie bei innerText
+// jumbo.textContent += string_3;              // Siehe innerText
