78b1d3863ffc80f34eecea5d8d5d1580e04a8977
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-26 09:06:53 +0200

    # querySelector...()-Methoden gelernt

---
+++ Document Objekt Model I (DOM)/querySelector...()-Methoden/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+    // Element mit CCS-Selektor auswählen
+let qs_1 = document.querySelector("body > #navigation > ul");
+console.log(qs_1);
+
+let qs_2 = document.querySelector(".jumbotron");    // querySelector -> gibt uns ein Element-Objekt zurück, für das ERSTE Element, was auf diesen Selektor passt
+console.log(qs_2);
+
+let qs_3 = document.querySelector(".test");
+console.log(qs_3);
+
+/*
+let qs_4 = document.querySelector("%67§ >");        // Test mit einem "nicht-validen" CSS-Selektor -> DOMException error
+console.log(qs_4);
+*/
+
+    // Elemente mit CSS-Selektor auswählen
+    
+let qsa_1 = document.querySelectorAll("body > #navigation > ul > li > a");
+console.log(qsa_1);
+
+let qsa_2 = document.querySelectorAll(".test");
+console.log(qsa_2);
+// console.log(qsa_2.length == 0);
+
+// let qsa_3 = document.querySelectorAll("%67§");      // erneut error
