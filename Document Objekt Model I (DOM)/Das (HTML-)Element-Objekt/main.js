1957e2699517a7e7c2b3f62524c50e201d851aa7
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-26 10:11:23 +0200

    # Das (HTML-)Element-Objekt gelernt

---
+++ Document Objekt Model I (DOM)/Das (HTML-)Element-Objekt/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let el = document.querySelector("body > nav > ul > li > a");
+console.log(el);
+
+// Weitere Infos: 
+//  Element:        https://developer.mozilla.org/en-US/docs/Web/API/Element            deutsch: https://wiki.selfhtml.org/wiki/JavaScript/DOM/Element
+//  HTML-Element:   https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement        deutsch: https://wiki.selfhtml.org/wiki/JavaScript/DOM/HTMLElement
