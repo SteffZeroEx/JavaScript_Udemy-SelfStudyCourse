d6bb755c73e95f97dad7ae7b20a79dd4633cd9a1
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-07-05 13:20:19 +0200

    # Cookies's Haltbarkeit inkl. löschen & überschreiben gelernt

--- Cookies und Daten-Speicher/Cookies/main.js
+++ Cookies und Daten-Speicher/Cookies/main.js
@@ -1,28 +1,45 @@
-'use strict';
+"use strict";
 
-// console.log(document.cookie);
 
-// Syntax:
+// Syntax Orig:
 // document.cookie = "cookie_name=[cookie_wert]"
+
+// Cookies setzen
 
 document.cookie = "vorname=Steffen";
 document.cookie = "nachname=Kieske";
 document.cookie = `${encodeURIComponent("ein key;value-paare")}=${encodeURIComponent("key=value")}`;
 
-const set_cookie = function (name, wert) {
-    let cookie = `${encodeURIComponent(name)}=`;
-    if (wert) {
-        cookie += `${encodeURIComponent(wert)}`;
-    }
-    document.cookie = cookie;
+// Syntax Haltbarkeit:
+// document.cookie = "cookie_name=[cookie_wert][; max-age=zeit_in_s]"  -> Haltbarkeit setzen
+document.cookie = "10s_cookie=; max-age=10";
+setTimeout(() => console.log(document.cookie), 9000);
+setTimeout(() => console.log(document.cookie), 11000);
+document.cookie = `mein_alter=32; max-age=${60*60*24*365}`;
+
+const set_cookie = function (name, wert, haltbarkeit) {
+  let cookie = `${encodeURIComponent(name)}=`;
+  if (wert) {
+    cookie += `${encodeURIComponent(wert)}`;
+  }
+  if (haltbarkeit) {
+    cookie += `; max-age=${haltbarkeit}`;
+  }
+  document.cookie = cookie;
+};
+set_cookie("password", "%A$g[]hm;§8", 60*60*2);
+
+
+
+// Cookies überschreiben
+
+set_cookie("zahlen", "1234", 60*60);
+set_cookie("zahlen", "5678", 60*60*1,5);
+
+// Cookies löschen
+const delete_cookie = function (name) {
+    document.cookie = `${encodeURIComponent(name)}=; max-age=-1`
 }
-set_cookie("password", "%A$g[]hm;§8");
-
-
-
-
+delete_cookie("ein key;value-paare");
 
 console.log(document.cookie);
-// - _ . ! ~ * ' ( )
-
-
